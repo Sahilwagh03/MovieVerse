c91d5fcdc6a65aa139cbd3d7ab96cdff960fadb4
sahilwagh03 <sahilwagh142@gmail.com>
2023-04-25 00:13:23 +0530

    Initial commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Header.css'
+import { Link } from 'react-router-dom'
+
+
+export const Header = () => {
+  return (
+    <div className="header">
+        <div className="headerLeft">
+            <Link to='/'><img className='header__icon' src="https://blogger.googleusercontent.com/img/a/AVvXsEi1ADDxYXjmNuDBrfs1JMo95osx_iSX6AC48_lJ_T2UFDzfuRjeDf4w1DA4QoxFg4caZwHEwu75zE-ntbzfAy8cBJwCYZ4QIYTlU-_Syj0GIUJD1TNpfh3Teg8d5rAN4RrsKN7DtKD-trqmLmD26ryvLDRkDsnPObxsu770MzY6TlzvW4lsLQ_zl-yd=s1600" style={{width:'max-content'}} height={40} alt="" /></Link>
+            <Link to="/movies/popular" className='text-style'><span>Popular</span></Link>
+                <Link to="/movies/top_rated" className='text-style'><span>Top Rated</span></Link>
+                <Link to="/movies/upcoming" className='text-style'><span>Upcoming</span></Link>
+        </div> 
+    </div>
+  )
+}
