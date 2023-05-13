f362e1ddb3096fd994901705c34926b4d2c3ac64
sahilwagh03 <sahilwagh142@gmail.com>
2023-05-13 20:05:23 +0530

    feat style

--- src/Pages/AnimeDetails/Anime.jsx
+++ src/Pages/AnimeDetails/Anime.jsx
@@ -9,6 +9,7 @@ export const Anime = () => {
   const [AnimeData, setAnimeData] = useState({})
   const [Characters, setCharacters] = useState([])
   const [showMore, setShowMore] = useState(false)
+  const [LoadMore, setLoadMore] = useState(false)
 
   const getAnime = (id) => {
     fetch(`https://api.jikan.moe/v4/anime/${id}`)
@@ -44,7 +45,7 @@ export const Anime = () => {
         <div className="details">
           <div className="detail">
             <div className="image">
-              <img src={images?.jpg.large_image_url} alt="" />
+              <img className='Anime-poster-img' src={images?.jpg.large_image_url} alt="" />
             </div>
             <div className="anime-details">
               <p><span>Aired:</span><span>{aired?.string}</span></p>
@@ -59,9 +60,9 @@ export const Anime = () => {
               <p><span>Duration:</span><span>{duration}</span></p>
             </div>
           </div>
-          <p className="description">
+          <p className="description anime_description">
             {showMore ? synopsis : synopsis?.substring(0, 450) + '...'}
-            <button onClick={() => {
+            <button className='readMore' onClick={() => {
               setShowMore(!showMore)
             }}>{showMore ? 'Show Less' : 'Read More'}</button>
           </p>
@@ -69,30 +70,50 @@ export const Anime = () => {
         <h3 className='title-trailer'>Trailer</h3>
         <div className="tralier-con">
           {
-            trailer?.embed_url && 
-            <iframe 
+            trailer?.embed_url &&
+            <iframe
               src={trailer?.embed_url}
               title='Inline Frame Example'
               width={800}
               height={450}
               allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
               allowFullScreen>
-              </iframe>
+            </iframe>
           }
         </div>
         <h3 className='title-characters'>Characters</h3>
         <div className="characters">
-          {Characters?.map((character , index) =>{
-              const {role}=character
-              const {images ,name ,mal_id} = character.character
+          {LoadMore ? (
+            Characters?.map((character, index) => {
+              const { role } = character
+              const { images, name, mal_id } = character.character
               return <Link className='links' to={`/character/${mal_id}`} key={index}>
                 <div className="character">
-                  <img src={images?.jpg.image_url} alt="" width={225} height={350}/>
+                  <img className='character-image' src={images?.jpg.image_url} alt="" width={225} height={350} />
                   <h4>{name}</h4>
                   <p>{role}</p>
                 </div>
               </Link>
-          })}
+            })
+          )
+            :
+            (
+              Characters?.slice(0, 7).map((character, index) => {
+                const { role } = character
+                const { images, name, mal_id } = character.character
+                return <Link className='links' to={`/character/${mal_id}`} key={index}>
+                  <div className="character">
+                    <img className='character-image' src={images?.jpg.image_url} alt="" width={225} height={350} />
+                    <h4>{name}</h4>
+                    <p>{role}</p>
+                  </div>
+                </Link>
+              })
+            )
+          }
+          <div className="LoadMore">
+            <button className='LoadMore-btn' onClick={() => setLoadMore(!LoadMore)}>{LoadMore ? "Load Less" : "Load More"}</button>
+          </div>
         </div>
       </div>
     </>
